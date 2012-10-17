3e471fcc18e13db4406e4ca722a5aa78efc909a3
Eriq Augustine <eaugusti@chromium.org>
2012-10-16 22:00:17 -0700

    new parsing is complete

--- parser.py
+++ parser.py
@@ -20,21 +20,16 @@ def cleanFile(fileObj):
       contents += line.strip() + '\n'
       line = fileObj.readline()
 
-   # span, body, html tags are not informative
+   # span, body, html, strong, and div tags are not informative
    contents = re.sub(allTagRegex('span'), '', contents, flags=re.IGNORECASE)
    contents = re.sub(allTagRegex('body'), '', contents, flags=re.IGNORECASE)
    contents = re.sub(allTagRegex('html'), '', contents, flags=re.IGNORECASE)
    contents = re.sub(allTagRegex('strong'), '', contents, flags=re.IGNORECASE)
    contents = re.sub(allTagRegex('div'), '', contents, flags=re.IGNORECASE)
 
-   # Aldrin's review seems to be the only ones that cannot be parsed like all the others.
+   # Aldrin and Connor's review seems to be the only ones that cannot be parsed like all the others.
    # If we just remove all the breaks, it should be ok.
-   #if (re.search(('(aldrin)|(montana)|' +
-   #               '(Mee Heng Low is a noodle house with a vague resemblance to Chinese noodles)|' +
-   #               '(Upper Crust Trattoria is an italian restaurant in San Luis Obispo)|'+
-   #               '(Moe\'s Smokehouse BBQ is a BBQ restaurant that primarily serves burgers)',
-   #               contents, flags=re.IGNORECASE))):
-   if (re.search('(montana)|(Mee Heng Low is a noodle house with a vague resemblance to Chinese noodles)|(Upper Crust Trattoria is an italian restaurant in San Luis Obispo)|(Moe\'s Smokehouse BBQ is a BBQ restaurant that primarily serves burgers)', contents, flags=re.IGNORECASE)):
+   if (re.search('(montana)|(Mee Heng Low is a noodle house with a vague resemblance to Chinese noodles)|(Upper Crust Trattoria is an italian restaurant in San Luis Obispo)|(Moe\'s Smokehouse BBQ is a BBQ restaurant that primarily serves burgers)|(Connor Lange)|(Firestone Grill is usually extremely busy during the evening)|(One of the most important aspects of F\.McLintocks is the atmosphere of the venue)', contents, flags=re.IGNORECASE)):
       contents = re.sub(allTagRegex('br'), ' ', contents, flags=re.IGNORECASE)
 
    contents = re.sub(allTagRegex('p'), '\n', contents, flags=re.IGNORECASE)
@@ -47,7 +42,6 @@ def cleanFile(fileObj):
    # e2 80 99 = right single quote
    # c3 a9 = e with accent aigu
    # e2 80 93 = EN dash
-
    contents = re.sub('\xc2\xa0', '', contents)
    contents = re.sub('\xe2\x80\x9c', '"', contents)
    contents = re.sub('\xe2\x80\x9d', '"', contents)
@@ -61,15 +55,12 @@ def cleanFile(fileObj):
       line = line.strip()
       if len(line) != 0:
          # If this is the not the first line, and it is a reviewers name. Then put in another \n.
-         if len(cleanContents) != 0 and line.startswith('REVIEWER'):
-            cleanContents += '\n'
+         #if len(cleanContents) != 0 and line.startswith('REVIEWER'):
+         #   cleanContents += '\n'
 
          cleanContents += line + '\n'
 
-   # TEST
-   sys.stdout.write(cleanContents)
-
-   return cleanContents
+   return re.sub('\n$', '', cleanContents)
 
 def consValueRegex(name):
    return '{0}:\s*([^\<]+)(\<)?'.format(name)
@@ -94,40 +85,51 @@ def grabPara(lastPara, fileObj):
       sys.exit(1)
    return match.group(1)
 
-def readReview(fileObj):
-   if fileObj.closed:
-      return False
+def getConsValue(objectKey, textValue, line, review):
+   match = re.search(consValueRegex(textValue), line)
+   if not match:
+      raise
+   review[objectKey] = match.group(1)
 
-   review = {}
-   review['reviewer'] = grabConsValue('REVIEWER', fileObj)
-   review['restaurant'] = grabConsValue('NAME', fileObj)
+# Always returns an array if revirews.
+# Expects cleaned input
+def stringToReviews(content, baseFile):
+   rtn = []
 
-   # Consume address and city
-   fileObj.readline()
-   fileObj.readline()
+   lines = content.split('\n')
+   for i in range(0, int(len(lines) / 13)):
+      try:
+         review = {}
 
-   review['foodScore'] = grabConsValue('FOOD', fileObj)
-   review['serviceScore'] = grabConsValue('SERVICE', fileObj)
-   review['venueScore'] = grabConsValue('VENUE', fileObj)
-   review['overallScore'] = grabConsValue('RATING', fileObj)
+         getConsValue('reviewer', 'REVIEWER', lines[i * 13 + 0], review)
+         getConsValue('restaurant', 'NAME', lines[i * 13 + 1], review)
 
-   # Consume the "WRITTEN REVIEW" line
-   fileObj.readline()
+         # Consume address and city
+         # lines[i * 13 + 2]
+         # lines[i * 13 + 3]
 
-   review['foodReview'] = grabPara(False, fileObj)
-   review['serviceReview'] = grabPara(False, fileObj)
-   review['venueReview'] = grabPara(False, fileObj)
-   review['overallReview'] = grabPara(False, fileObj)
+         getConsValue('foodScore', 'FOOD', lines[i * 13 + 4], review)
+         getConsValue('serviceScore', 'SERVICE', lines[i * 13 + 5], review)
+         getConsValue('venueScore', 'VENUE', lines[i * 13 + 6], review)
+         getConsValue('overallScore', 'RATING', lines[i * 13 + 7], review)
 
-   # Consume a final line, this will do nothing if it is one review per file,
-   #  but if there are multiple reviews in a file, it will take the sep space.
-   line = fileObj.readline()
-   if len(line) == 0:
-      fileObj.close()
+         # Consume the "WRITTEN REVIEW" line
+         # lines[i * 13 + 8]
 
-   fileObj.close()
+         # Paragraphs have already been properly formatted and stripped
+         review['foodReview'] = lines[i * 13 + 9]
+         review['serviceReview'] = lines[i * 13 + 10]
+         review['venueReview'] = lines[i * 13 + 11]
+         review['overallReview'] = lines[i * 13 + 12]
 
-   return review
+         review['file'] = baseFile
+         review['position'] = i
+
+         rtn.append(review)
+      except:
+         pass
+
+   return rtn
 
 def readReviews(targetDir):
    reviews = []
@@ -141,19 +143,8 @@ def readReviews(targetDir):
       fileObj = open(fileName, 'r')
 
       content = cleanFile(fileObj)
-      continue
+      reviews += stringToReviews(content, baseFile)
 
-      count = 0
-
-      while True:
-         review = readReview(fileObj)
-         if not review:
-            break
-         review['file'] = baseFile
-         review['position'] = count
-         reviews.append(review)
-
-         count += 1
    return reviews
 
 def readAllReviews():
@@ -169,4 +160,4 @@ if __name__ == '__main__':
    #fileObj = open(sys.argv[1], 'r')
    #cleanFile(fileObj)
    reviews = readAllReviews()
-   #print reviews
+   print reviews
